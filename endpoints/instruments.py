cf39932339d99106a7830e336bf78161d458734a
tacchangxyz <user001@tacchang.xyz>
2019-03-17 09:56:20 +0900

    pythonで関数オーバロード使えないかなって思って思ったけど諦める。

--- endpoints/instruments.py
+++ endpoints/instruments.py
@@ -16,35 +16,6 @@ def get_candles(instrument, params):
     return _request.response
 
 
-def make_from_to_params(granularity, from_time, to_time):
-    _params = {
-        "from": from_time.strftime(_TIMESTAMP_FORMAT),
-        "to": to_time.strftime(_TIMESTAMP_FORMAT),
-        "granularity": granularity
-    }
-
-    return _params
-
-
-def make_to_params(granularity, to_time):
-    _params = {
-        "to": to_time.strftime(_TIMESTAMP_FORMAT),
-        "granularity": granularity
-    }
-
-    return _params
-
-
-def make_from_count_params(granularity, from_time, count):
-    _params = {
-        "from": from_time.strftime(_TIMESTAMP_FORMAT),
-        "count": count,
-        "granularity": granularity
-    }
-
-    return _params
-
-
 def to_dataframe(candle_dict):
     candle = pd.DataFrame.from_dict([row['mid'] for row in candle_dict['candles']])
     candle['time'] = [row['time'] for row in candle_dict['candles']]
@@ -56,29 +27,69 @@ def to_dataframe(candle_dict):
     return candle
 
 
-def _time_str(date_time):
-    return date_time.strftime(_TIMESTAMP_FORMAT)
+def make_instruments_params(granularity, from_time=None, to_time=None, count=None):
+    if from_time is not None and to_time is not None:
+        _params = _make_from_to_params(granularity, from_time=from_time, to_time=to_time)
+    elif from_time is None and to_time is not None and count is None:
+        _params = _make_to_params(granularity, to_time=to_time)
+    elif from_time is not None and to_time is None and count is not None:
+        _params = _make_from_count_params(granularity, from_time=from_time, count=count)
+    else:
+        _params = {
+            "granularity": granularity
+        }
+
+    return _params
+
+
+def _make_from_to_params(granularity, from_time, to_time):
+    _params = {
+        "from": from_time.strftime(_TIMESTAMP_FORMAT),
+        "to": to_time.strftime(_TIMESTAMP_FORMAT),
+        "granularity": granularity
+    }
+
+    return _params
+
+
+def _make_to_params(granularity, to_time):
+    _params = {
+        "to": to_time.strftime(_TIMESTAMP_FORMAT),
+        "granularity": granularity
+    }
+
+    return _params
+
+
+def _make_from_count_params(granularity, from_time, count):
+    _params = {
+        "from": from_time.strftime(_TIMESTAMP_FORMAT),
+        "count": count,
+        "granularity": granularity
+    }
+
+    return _params
 
 
 def main():
     from datetime import datetime
 
     _to1 = datetime(2018, 4, 10, 12, 34, 56)
-    _params1 = make_to_params("M15", to_time=_to1)
+    _params1 = make_instruments_params("M15", to_time=_to1)
     _candle_dict1 = get_candles("USD_JPY", _params1)
     _candles1 = to_dataframe(_candle_dict1)
     print(_candles1)
 
     _from2 = datetime(2018, 4, 10, 0, 0, 0)
     _to2 = datetime(2018, 4, 10, 12, 34, 56)
-    _params2 = make_from_to_params("M15", from_time=_from2, to_time=_to2)
+    _params2 = make_instruments_params("M15", from_time=_from2, to_time=_to2)
     _candle_dict2 = get_candles("USD_JPY", _params2)
     _candles2 = to_dataframe(_candle_dict2)
     print(_candles2)
 
     _from3 = datetime(2018, 4, 10, 0, 0, 0)
     _count3 = 30
-    _params3 = make_from_count_params("M15", from_time=_from3, count=_count3)
+    _params3 = make_instruments_params("M15", from_time=_from3, count=_count3)
     _candle_dict3 = get_candles("USD_JPY", _params3)
     _candles3 = to_dataframe(_candle_dict3)
     print(_candles3)
