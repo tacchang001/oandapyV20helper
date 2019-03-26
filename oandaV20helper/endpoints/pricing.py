206225c3c9289fb9b7a9b9e83b43b734ad588c27
tacchangxyz <user001@tacchang.xyz>
2019-03-26 17:39:28 +0900

    asks/bidsはリスト（要素が複数）である点に対応

--- oandaV20helper/endpoints/pricing.py
+++ oandaV20helper/endpoints/pricing.py
@@ -13,6 +13,22 @@ pd.options.display.colheader_justify = 'right'
 _TIMESTAMP_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
 
 
+def _average_of_price(prices):
+    if len(prices) == 1:
+        price = float(prices[0]['price'])
+        volume = int(prices[0]['liquidity'])
+    else:
+        _sum = 0.0
+        volume = 0.0
+        for i in prices:
+            d = int(i['liquidity'])
+            _sum = _sum + (float(i['price']) * d)
+            volume = volume + d
+        price = _sum / volume
+
+    return price, volume
+
+
 def to_dataframe(raw_dict):
     candle = None
     if raw_dict['type'] == 'PRICE':
@@ -22,9 +38,11 @@ def to_dataframe(raw_dict):
         candle['instrument'] = raw_dict['instrument']
         candle['time'] = raw_dict['time']
         candle['time'] = pd.to_datetime(candle['time'], format=_TIMESTAMP_FORMAT)
-        candle['asks'] = [row['price'] for row in raw_dict['asks']]
-        candle['bids'] = [row['price'] for row in raw_dict['bids']]
+        candle['asks'], candle['volumeAsk'] = _average_of_price(raw_dict['asks'])
+        candle['bids'], candle['volumeBid'] = _average_of_price(raw_dict['bids'])
         candle['closeoutBid'] = raw_dict['closeoutBid']
         candle['closeoutAsk'] = raw_dict['closeoutAsk']
 
     return candle
+
+
