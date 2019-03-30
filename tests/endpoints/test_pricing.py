bfab9d22ab7d2e15e6e1ae0adc63407ff3023786
tacchangxyz <user001@tacchang.xyz>
2019-03-30 22:15:40 +0900

    ユニットテストのフレームワークをunittest→pytestに変更してみた。

--- tests/endpoints/test_pricing.py
+++ tests/endpoints/test_pricing.py
@@ -1,19 +1,22 @@
-from unittest import TestCase
 import json
+
+import pytest
 
 from oandaV20helper.endpoints.pricing import to_dom
 
 
-class TestPricing(TestCase):
+class TestPricing:
+    @pytest.mark.parametrize("num, filename", [
+        (1, 'test_pricing01.json'),
+        (3, 'test_pricing02.json'),
+        (15, 'test_pricing03.json')
+    ])
+    def test_to_dataframe(self, num, filename):
+        with open(filename, 'r') as f:
+            raw_dict = json.load(f)
+            result = to_dom(raw_dict)
+            print(result)
 
-    def test_to_dataframe(self):
-        data_dict = [
-            'test_pricing01.json',
-            'test_pricing02.json',
-            'test_pricing03.json'
-        ]
-        for _file_name in data_dict:
-            with open(_file_name, 'r') as f:
-                raw_dict = json.load(f)
-                result = to_dom(raw_dict)
-                print(result)
+
+if __name__ == '__main__':
+    pytest.main(['-v', __file__])
